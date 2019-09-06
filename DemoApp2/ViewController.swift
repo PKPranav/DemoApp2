69f381ce2705b29c1c053b2c6c9fc7bc75d5e4b6
Pramod <pramodkumar103@gmail.com>
2019-09-06 13:00:10 +0530

    Initial Commit

---
+++ DemoApp2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DemoApp2
+//
+//  Created by Pramod Kumar Pranav on 06/09/19.
+//  Copyright © 2019 Pramod Kumar Pranav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
